59a7b6f2145fa6457b0e12390efd8a642e89ab2d
unknown <1456421562@qq.com>
2020-06-08 23:49:53 +0800

    feat HttpBasicAuth 传递令牌

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,11 +1,14 @@
 const Router = require('koa-router')
-const router = new Router()
+const router = new Router({
+  prefix: '/v1/classic'
+})
 const {
   HttpException,
   ParameterException
 } = require('../../../core/http-exception')
 const { PositiveIntegerValidator } = require('../../validators/validators')
-router.post('/v1/:id/classic/latest', async (ctx, next) => {
+const { Auth } = require('../../../middlewares/auth')
+router.get('/latest',  new Auth().m ,async (ctx, next) => {
   const {
     header,
     body,
