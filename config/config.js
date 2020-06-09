3c4e00851fc793c03d7f47661196152870dea9b3
unknown <1456421562@qq.com>
2020-06-09 23:57:55 +0800

    feat 微信 appIDD

--- config/config.js
+++ config/config.js
@@ -13,7 +13,7 @@ module.exports = {
     expiresIn: 60 * 60 * 24 * 30
   },
   wx: {
-    appID: '0ced3fc5b101e435d7b73f13b553b258',
+    appID: 'wxab28fe99a0747361',
     appSecret: '0ced3fc5b101e435d7b73f13b553b258',
     loginUrl: 'https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code'
   }
