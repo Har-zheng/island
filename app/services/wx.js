7e8da91e11f5ba4cf4982c400371a5360fb81cbf
zhz <1456421562@qq.com>
2020-06-10 18:10:24 +0800

    feat 微信openid

--- app/services/wx.js
+++ app/services/wx.js
@@ -1,5 +1,22 @@
-class WXManger{
-  static async codeToToken(code){
-    
+const util = require('util')
+const axios = require('axios')
+const {
+  AuthFailed
+} = require('../../core/http-exception')
+class WXManger {
+  static async codeToToken(code) {
+    const url = util.format(global.config.wx.loginUrl,
+      global.config.wx.appId,
+      global.config.wx.appSecret
+    )
+    const result = await axios.get(url)
+    if (result.status !== 200) {
+      throw new AuthFailed('openid获取失败')
+    }
+    const errcode = result.data.errcode
+    if (errcode !== 0) {
+      throw new AuthFailed('openid获取失败' + errcode)
+    }
+
   }
 }
